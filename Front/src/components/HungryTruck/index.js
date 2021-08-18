02796859b6b9c5c1253f614beadbe77ff2281b5d
TURPINJonathan <turpin.j@hotmail.fr>
2021-08-18 18:13:11 +0200

    [ADD] Footer (static version)

--- Front/src/components/HungryTruck/index.js
+++ Front/src/components/HungryTruck/index.js
@@ -1,12 +1,15 @@
 // == Import npm
 import React from 'react';
 
+import Footer from '../Footer';
+
 // == Import
 import './styles.scss';
 
 // == Composant
 const HungryTruck = () => (
   <div className="hungrytruck">
+    <Footer />
   </div>
 );
 
