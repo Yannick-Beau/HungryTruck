e187dcbee61f1801840fa30870d0006c10d3bd33
TURPINJonathan <turpin.j@hotmail.fr>
2021-08-18 16:11:51 +0200

    [ADD] React instal && favicon

---
+++ Front/src/components/HungryTruck/index.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './styles.scss';
+
+// == Composant
+const HungryTruck = () => (
+  <div className="hungrytruck">
+    <h1>Composant : App</h1>
+  </div>
+);
+
+// == Export
+export default HungryTruck;
