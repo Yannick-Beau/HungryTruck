6e286bc0ff5e15c95b96f03902b474cf7f78e7e0
ExoCinki <aolacot@gmail.com>
2021-08-18 20:59:11 +0200

    [ADD] Entity User

---
+++ Back/migrations/Version20210818185849.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210818185849 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD pseudo VARCHAR(100) NOT NULL, ADD avatar VARCHAR(255) DEFAULT NULL, ADD cp INT NOT NULL, ADD city VARCHAR(255) NOT NULL, ADD adresse VARCHAR(255) NOT NULL, ADD siret INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP pseudo, DROP avatar, DROP cp, DROP city, DROP adresse, DROP siret');
+    }
+}
