2a2e1417768f0b8b25b925f655904471bc62ca4b
TURPINJonathan <turpin.j@hotmail.fr>
2021-08-19 18:27:05 +0200

    [UPDATE] Main's CSS

--- Front/src/components/Main/Introduction/index.js
+++ Front/src/components/Main/Introduction/index.js
@@ -7,11 +7,11 @@ import './introduction.scss';
 // == Composant
 const Introduction = () => (
   <aside className="introduction">
-    <h2> Bienvenu sur HungryTruck</h2>
-    <p>
+    <h2 className="introduction-title"> Bienvenu sur HungryTruck</h2>
+    <p className="introduction-text">
       Ce site vous permettra de visualiser les différents food trucks qui sont autour de chez vous, ainsi que les menus.
     </p>
-    <p>De quoi vous régaler et dire adieu à votre balance !</p>
+    <p className="introduction-text">De quoi vous régaler et dire adieu à votre balance !</p>
   </aside>
 );
 
