2127113b9364d9806e8aca1b8c16fb7c62c82d3e
Yannick-Beau <beauyannick@gmail.com>
2021-08-19 14:18:05 +0200

    [UPDATE] clear code

--- Front/src/components/HungryTruck/index.js
+++ Front/src/components/HungryTruck/index.js
@@ -1,11 +1,10 @@
 // == Import npm
 import React from 'react';
 
-Front-Nav
+
 // == Import components
 import Nav from '../Nav';
 import Footer from '../Footer';
-Front
 
 // == Import
 import './styles.scss';
