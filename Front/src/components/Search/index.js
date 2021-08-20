3833bf0b74e17a33dde73d5b108d55369f31c940
TURPINJonathan <turpin.j@hotmail.fr>
2021-08-20 14:02:06 +0200

    [UPDATE] search CSS desktop version DONE

--- Front/src/components/Search/index.js
+++ Front/src/components/Search/index.js
@@ -7,56 +7,58 @@ import Logo from '../../assets/img/logo.png';
 
 // == Composant
 const Search = () => (
-  <div className="search">
+  <>
     <h3 className="search-title">
       Recherche
     </h3>
-    <form className="search-form">
-      <label className="search-form--label" htmlFor="search">
-        <div className="search-form-inputSubmit">
-          <input type="text" name="search" className="search-form--input" placeholder="Veuillez saisir votre rechercher" />
-          <input type="submit" className="search-form--submit" value="Manger !" />
-        </div>
-      </label>
-    </form>
-    <section className="search-results">
-      <p className="search-results--title">Résultat(s) de votre recherche :</p>
+    <div className="search">
 
-      <article className="search-results--result">
-        <img src={Logo} className="result-logo" alt="logo" />
-        <div className="result-details">
-          <p className="result-title">
-            Nom du truck
-          </p>
-          <p className="result-type">
-            Type de food
-          </p>
-          <p className="result-time">
-            <p>Lundi: 16h / 22h - Place des grands Hommes</p>
-            <p>Lundi: 16h / 22h - Place des grands Hommes</p>
-          </p>
-        </div>
-      </article>
+      <form className="search-form">
+        <label className="search-form--label" htmlFor="search">
+          <div className="search-form-inputSubmit">
+            <input type="text" name="search" className="search-form--input" placeholder="Veuillez saisir votre rechercher" />
+            <input type="submit" className="search-form--submit" value="Manger !" />
+          </div>
+        </label>
+      </form>
+      <section className="search-results">
+        <p className="search-results--title">Résultat(s) de votre recherche :</p>
 
+        <article className="search-results--result">
+          <img src={Logo} className="result-logo" alt="logo" />
+          <div className="result-details">
+            <p className="result-title">
+              Nom du truck
+            </p>
+            <p className="result-type">
+              Type de food
+            </p>
+            <p className="result-time">
+              <p>Lundi: 16h / 22h - Place des grands Hommes</p>
+              <p>Lundi: 16h / 22h - Place des grands Hommes</p>
+            </p>
+          </div>
+        </article>
 
-      <article className="search-results--result">
-        <img src={Logo} className="result-logo" alt="logo" />
-        <div className="result-details">
-          <p className="result-title">
-            Nom du truck
-          </p>
-          <p className="result-type">
-            Type de food
-          </p>
-          <p className="result-time">
-            <p>Lundi: 16h / 22h - Place des grands Hommes</p>
-            <p>Lundi: 16h / 22h - Place des grands Hommes</p>
-          </p>
-        </div>
-      </article>
+        <article className="search-results--result">
+          <img src={Logo} className="result-logo" alt="logo" />
+          <div className="result-details">
+            <p className="result-title">
+              Nom du truck
+            </p>
+            <p className="result-type">
+              Type de food
+            </p>
+            <p className="result-time">
+              <p>Lundi: 16h / 22h - Place des grands Hommes</p>
+              <p>Lundi: 16h / 22h - Place des grands Hommes</p>
+            </p>
+          </div>
+        </article>
 
-    </section>
-  </div>
+      </section>
+    </div>
+  </>
 );
 
 // == Export
