dd0c8d106f2d869314882d0c007b99b78a76ec11
TURPINJonathan <turpin.j@hotmail.fr>
2021-08-21 00:09:37 +0200

    [ADD] Try a version for who are we page

--- Front/src/components/HungryTruck/index.js
+++ Front/src/components/HungryTruck/index.js
@@ -10,6 +10,7 @@ import Footer from '../Footer';
 import Main from '../Main';
 import LegalMentions from '../Footer/LegalMentions';
 import Search from '../Search';
+import WhoAreWe from '../WhoAreWe';
 
 // == Import
 import './styles.scss';
@@ -27,6 +28,9 @@ const HungryTruck = () => (
       </Route>
       <Route path="/search" exact>
         <Search />
+      </Route>
+      <Route path="/who-are-we" exact>
+        <WhoAreWe />
       </Route>
     </Switch>
     <Footer />
---
+++ Front/src/components/WhoAreWe/index.js
@@ -0,0 +1,51 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './whoarewe.scss';
+import Yannick from '../../assets/img/yannick.jpg';
+import John from '../../assets/img/john.jpg';
+import Aodren from '../../assets/img/aodren.jpg';
+
+// == Composant
+const WhoAreWe = () => (
+  <section className="team">
+    <div className="team-front">
+      <article className="team-card">
+        {/* <div className="team-card--complete"> */}
+          <img src={John} alt="john" className="team-card--john" />
+          <div className="team-card--details_john">
+            <p>TURPIN Jonathan</p>
+            <p>Product Owner</p>
+            <p>Développeur Front</p>
+          </div>
+        {/* </div> */}
+      </article>
+      <article className="team-card">
+        {/* <div className="team-card--complete"> */}
+          <img src={Yannick} alt="yannick" className="team-card-yannick" />
+          <div className="team-card--details_yannick">
+            <p>BEAU Yannick</p>
+            <p>Lead Dev Front / Scrum Master</p>
+            <p>Développeur Front</p>
+          </div>
+        {/* </div> */}
+      </article>
+    </div>
+    <div className="team-back">
+      <article className="team-card">
+        {/* <div className="team-card--complete"> */}
+          <img src={Aodren} alt="aodren" className="team-card--aodren" />
+          <div className="team-card--details_aodren">
+            <p>LLC Aodrën</p>
+            <p>Lead Dev Back / Git Master</p>
+            <p>Développeur Back</p>
+          </div>
+        {/* </div> */}
+      </article>
+    </div>
+  </section>
+);
+
+// == Export
+export default WhoAreWe;
