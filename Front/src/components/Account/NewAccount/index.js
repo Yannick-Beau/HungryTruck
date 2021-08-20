c4e7394363244ae04680fc2b33557943ec5e3e85
Yannick-Beau <beauyannick@gmail.com>
2021-08-20 18:10:32 +0200

    [UPDATE] NewAccount -> css wip it remains to do the toggle and submit button, media queries

--- Front/src/components/Account/NewAccount/index.js
+++ Front/src/components/Account/NewAccount/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 
 // == Import
-import imgUser from '../../../assets/img/icon-user.png';
+import avatar from '../../../assets/img/avatar.jpg';
 import './newAccount.scss';
 
 // == Composant
@@ -11,8 +11,8 @@ const NewAccount = () => (
     <h1 className="newaccount-title">Créer votre compte [utilisateur] [pro] </h1>
     <form className="newaccount-form">
       <div className="form-left">
-        <img className="avatar" src={imgUser} alt="Avatar" />
-        <label className="switch-form" htmlFor="switch">Lien de votre image de profil
+        <img className="avatar" src={avatar} alt="Avatar" />
+        <label className="form-label--avatar" htmlFor="avatar">Lien de votre image de profil*
           <input
             type="text"
             className="avatar-input"
@@ -23,70 +23,101 @@ const NewAccount = () => (
       </div>
       <div className="form-right">
         <label className="switch-form" htmlFor="pro">
-          <h2>Vous êtes professionnel ?</h2>
+          <h2>Vous êtes professionnel</h2>
           <input type="checkbox" name="pro" className="switch" />
           <span className="slider round" />
         </label>
-        <div className="fields">
-          <div className="fields-left">
-            <div className="field">
-              <label className="field-label" htmlFor="email">Saisissez votre Email
-                <input className="field-input" type="text" name="email" id="field-input--email" placeholder="john.doeuf@gmail.com" />
-              </label>
+        <h2>Vos informations personnelles</h2>
+        <p className="fields-legend"><span>* Champ obligatoire</span></p>
+        <div className="all-fields">
+          <div className="fields">
+            <div className="fields-left">
+              <div className="field">
+                <label className="field-label" htmlFor="email">Saisissez votre Email
+                  <div>
+                    <input className="field-input" type="text" name="email" id="field-input--email" placeholder="john.doeuf@gmail.com" />
+                    <span>*</span>
+                  </div>
+                </label>
+              </div>
+              <div className="field">
+                <label className="field-label" htmlFor="pseudo">Saisissez votre Pseudo
+                  <div>
+                    <input className="field-input" type="text" name="pseudo" id="field-input--pseudo" placeholder="Poule" />
+                    <span>*</span>
+                  </div>
+                </label>
+              </div>
+              <div className="field">
+                <label className="field-label" htmlFor="password">Saisissez votre mot de passe
+                  <div>
+                    <input className="field-input" type="password" name="password" id="field-input--password" />
+                    <span>*</span>
+                  </div>
+                </label>
+              </div>
+              <div className="field">
+                <label className="field-label" htmlFor="password-confirmation">Confirmer votre mot de passe
+                  <div>
+                    <input className="field-input" type="password" name="password-confirmation" />
+                    <span>*</span>
+                  </div>
+                </label>
+              </div>
             </div>
-            <div className="field">
-              <label className="field-label" htmlFor="pseudo">Saisissez votre Pseudo
-                <input className="field-input" type="text" name="pseudo" id="field-input--pseudo" placeholder="Poule" />
-              </label>
+            <div className="fields-right">
+              <div className="field">
+                <label className="field-label" htmlFor="adresse">Saisissez votre adresse
+                  <div>
+                    <input className="field-input" type="text" name="adresse" id="field-input--adresse" placeholder="3 rue de paris" />
+                    <span>*</span>
+                  </div>
+                </label>
+              </div>
+              <div className="field">
+                <label className="field-label" htmlFor="postal-code">Saisissez votre code postal
+                  <div>
+                    <input className="field-input" type="text" name="postal-code" placeholder="69000" />
+                    <span>*</span>
+                  </div>
+                </label>
+              </div>
+              <div className="field">
+                <label className="field-label" htmlFor="city">Saisissez votre ville
+                  <div>
+                    <input className="field-input" type="text" name="city" placeholder="Lyon" />
+                    <span>*</span>
+                  </div>
+                </label>
+              </div>
+              { true
+              && (
+              <div className="field">
+                <label className="field-label" htmlFor="siret">Saisissez votre n° de SIRET
+                  <div>
+                    <input className="field-input" type="text" name="siret" placeholder="12345678900012" />
+                    <span>*</span>
+                  </div>
+                </label>
+              </div>
+              )}
             </div>
-            <div className="field">
-              <label className="field-label" htmlFor="password">Saisissez votre mot de passe
-                <input className="field-input" type="password" name="password" id="field-input--password" />
-              </label>
-            </div>
-            <div className="field">
-              <label className="field-label" htmlFor="password-confirmation">Confirmer votre mot de passe
-                <input className="field-input" type="password" name="password-confirmation" />
-              </label>
+            <div className="fields-food">
+              <h2>Choissier vos nourritures favorites</h2>
+              <div className="field-check">
+                <label className="field-label" htmlFor="pizza">Pizza
+                  <input type="checkbox" name="pizza" className="switch" />
+                  <span className="slider round" />
+                </label>
+                <label className="field-label" htmlFor="burger">Burger
+                  <input type="checkbox" name="burger" className="switch" />
+                  <span className="slider round" />
+                </label>
+              </div>
             </div>
           </div>
-          <div className="fields-right">
-            <div className="field">
-              <label className="field-label" htmlFor="adresse">Saisissez votre adresse
-                <input className="field-input" type="text" name="adresse" id="field-input--adresse" placeholder="3 rue de paris" />
-              </label>
-            </div>
-            <div className="field">
-              <label className="field-label" htmlFor="postal-code">Saisissez votre code postal
-                <input className="field-input" type="text" name="postal-code" placeholder="69000" />
-              </label>
-            </div>
-            <div className="field">
-              <label className="field-label" htmlFor="city">Saisissez votre ville
-                <input className="field-input" type="text" name="city" placeholder="Lyon" />
-              </label>
-            </div>
-            { true
-            && (
-            <div className="field">
-              <label className="field-label" htmlFor="city">Saisissez votre ville
-                <input className="field-input" type="text" name="city" placeholder="Lyon" />
-              </label>
-            </div>
-            )}
-          </div>
         </div>
-        <div className="fiels-food">
-          <h2>Choissier vos nourritures favorite</h2>
-          <label className="field-label" htmlFor="pizza">Pizza
-            <input type="checkbox" name="pizza" className="switch" />
-            <span className="slider round" />
-          </label>
-          <label className="field-label" htmlFor="burger">Burger
-            <input type="checkbox" name="burger" className="switch" />
-            <span className="slider round" />
-          </label>
-        </div>
+        <button className="submit-form" type="submit" value="Submit">Créer mon compte [pro]</button>
       </div>
     </form>
   </main>
