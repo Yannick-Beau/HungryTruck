86643cb93225b4485b67ad52d5ac4eb75ac66355
TURPINJonathan <turpin.j@hotmail.fr>
2021-08-21 01:07:34 +0200

    [UPDATE] Contact form desktop version DONE

--- Front/src/components/Contact/index.js
+++ Front/src/components/Contact/index.js
@@ -6,9 +6,28 @@ import './contact.scss';
 
 // == Composant
 const Contact = () => (
-  <aside className="introduction">
-    Je suis dans contact
-  </aside>
+  <section className="contact">
+    <h2 className="contact-title">Contactez-nous</h2>
+    <article>
+      <form>
+        <p>Vous souhaitez nous adresser un message ?</p>
+        <label htmlFor="mail">
+          Veuillez renseigner votre adresse mail :
+          <input type="text" name="mail" placeholder="exemple : john@deuf.fr" />
+        </label>
+        <label htmlFor="message">
+          Quel message souhaitez vous nous adresser ?
+          <textarea
+            name="message"
+            type="text"
+            placeholder="Ecrivez votre message ici"
+            minLength="20"
+          />
+        </label>
+        <button type="submit">Envoyer mon message</button>
+      </form>
+    </article>
+  </section>
 );
 
 // == Export
