9c11ac8da67cf19141aabc60d3693d729fab288b
TURPINJonathan <turpin.j@hotmail.fr>
2021-08-22 01:19:25 +0200

    [UPDATE] static desktop version

--- Front/src/components/MyAccount/index.js
+++ Front/src/components/MyAccount/index.js
@@ -6,8 +6,43 @@ import './myaccount.scss';
 
 // == Composant
 const MyAccount = () => (
-  <section className="account">
-    Je suis dans mon compte
+  <section className="sectionAccount">
+    <h2 className="myAccount">Mon compte</h2>
+    <img
+      className="myaccount-picture"
+      src="http://placehold.it/250x350"
+      alt="mon compte hungrytruck"
+    />
+    <article className="account">
+      <div className="account-informations">
+        <div className="informations-left">
+          <p>Mon pseudo : Turpinou</p>
+          <p>Mon adresse :</p>
+          <p>15 place des grands hommes</p>
+          <p>12345 IDEFIX</p>
+          <p>SIRET : 123456789</p>
+        </div>
+        <div className="informations-right">
+          <p>Mon adresse mail: turpinou@idefix.fr</p>
+          <p>facebook</p>
+          <p>insta</p>
+          <p>Mes plats favoris :</p>
+          <ul>
+            <li>pizza</li>
+            <li>burger</li>
+          </ul>
+        </div>
+      </div>
+      <div className="account-button">
+        <button type="button" className="button-edit">
+          Editer mes informations
+        </button>
+        <button type="button" className="button-back">
+          Retour au menu principal
+        </button>
+      </div>
+    </article>
+
   </section>
 );
 
