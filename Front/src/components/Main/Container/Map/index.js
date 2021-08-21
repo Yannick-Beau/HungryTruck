11966e09793d6fdaddf51b8c8b7c0497645fa30b
TURPINJonathan <turpin.j@hotmail.fr>
2021-08-21 13:25:17 +0200

    map && user marker work

--- Front/src/components/Main/Container/Map/index.js
+++ Front/src/components/Main/Container/Map/index.js
@@ -1,15 +1,42 @@
 // == Import npm
 import React from 'react';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+} from 'react-leaflet';
 
 // == Import
 import './map.scss';
 
 // == Composant
-const Map = () => (
-  <article className="map">
-    {/* Map */}
-  </article>
-);
+const Map = () => {
+  // position coordinates
+  const position = [43.505, -0.09];
+
+  return (
+    <div id="map">
+      <MapContainer
+        center={position}
+        zoom={13} // map zoom
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {/* User marker */}
+        <Marker position={position}>
+          {/* Popup user marker */}
+          <Popup>
+            Ici, c'est chez Aodren <br /> Tout le monde est invité !
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
 
 // == Export
 export default Map;
