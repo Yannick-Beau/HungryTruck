3f45848800626d7037696a71f88f10c4dd33c494
TURPINJonathan <turpin.j@hotmail.fr>
2021-08-24 09:18:07 +0200

    [ADD] NewFoodTruck component

---
+++ Front/src/components/MyAccount/MyFoodTruck/NewFoodTruck/index.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './newfoodtruck.scss';
+
+// == Composant
+const NewFoodTruck = () => (
+  <main className="main">
+    Je suis dans créer un food truck
+  </main>
+);
+
+// == Export
+export default NewFoodTruck;
