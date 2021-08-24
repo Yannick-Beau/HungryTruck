4cc88a9109841cb6cf6a547cc75bad602f34a7b0
Yannick-Beau <beauyannick@gmail.com>
2021-08-24 14:45:05 +0200

    [UPDATE] Search -> add link on article result

--- Front/src/components/Search/index.js
+++ Front/src/components/Search/index.js
@@ -1,5 +1,6 @@
 // == Import npm
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // == Import
 import './search.scss';
@@ -23,39 +24,44 @@ const Search = () => (
       </form>
       <section className="search-results">
         <p className="search-results--title">Résultat(s) de votre recherche :</p>
-
-        <article className="search-results--result">
-          <img src={Logo} className="result-logo" alt="logo" />
-          <div className="result-details">
-            <p className="result-title">
-              Nom du truck
-            </p>
-            <p className="result-type">
-              Type de food
-            </p>
-            <p className="result-time">
-              <p>Lundi: 16h / 22h - Place des grands Hommes</p>
-              <p>Lundi: 16h / 22h - Place des grands Hommes</p>
-            </p>
-          </div>
-        </article>
-
-        <article className="search-results--result">
-          <img src={Logo} className="result-logo" alt="logo" />
-          <div className="result-details">
-            <p className="result-title">
-              Nom du truck
-            </p>
-            <p className="result-type">
-              Type de food
-            </p>
-            <p className="result-time">
-              <p>Lundi: 16h / 22h - Place des grands Hommes</p>
-              <p>Lundi: 16h / 22h - Place des grands Hommes</p>
-            </p>
-          </div>
-        </article>
-
+        <Link
+          to="/food-truck"
+        >
+          <article className="search-results--result">
+            <img src={Logo} className="result-logo" alt="logo" />
+            <div className="result-details">
+              <p className="result-title">
+                Nom du truck
+              </p>
+              <p className="result-type">
+                Type de food
+              </p>
+              <p className="result-time">
+                <p>Lundi: 16h / 22h - Place des grands Hommes</p>
+                <p>Lundi: 16h / 22h - Place des grands Hommes</p>
+              </p>
+            </div>
+          </article>
+        </Link>
+        <Link
+          to="/food-truck"
+        >
+          <article className="search-results--result">
+            <img src={Logo} className="result-logo" alt="logo" />
+            <div className="result-details">
+              <p className="result-title">
+                Nom du truck
+              </p>
+              <p className="result-type">
+                Type de food
+              </p>
+              <p className="result-time">
+                <p>Lundi: 16h / 22h - Place des grands Hommes</p>
+                <p>Lundi: 16h / 22h - Place des grands Hommes</p>
+              </p>
+            </div>
+          </article>
+        </Link>
       </section>
     </div>
   </>
