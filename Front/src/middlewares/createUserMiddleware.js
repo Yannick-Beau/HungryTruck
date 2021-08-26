786a320404132c608e07057878da00f3602e38c2
TURPINJonathan <turpin.j@hotmail.fr>
2021-08-26 10:49:38 +0200

    [UPDATE] create account WIP

--- Front/src/middlewares/createUserMiddleware.js
+++ Front/src/middlewares/createUserMiddleware.js
@@ -4,18 +4,56 @@ import { CREATE_USER } from '../actions/createUser';
 const createUserMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
     case CREATE_USER: {
-      const { email, password } = store.getState().createUser;
+      const {
+        email,
+        password,
+        createPro,
+        nickname,
+        pictureUser,
+        cp,
+        city,
+        address,
+        siret,
+      } = store.getState().createUser;
+
+      const newCP = parseInt(cp, 10);
+      const newSiret = parseInt(siret, 10);
+      let proUser = ['ROLE_USER'];
+      if (createPro === true) {
+        proUser = ['ROLE_PRO'];
+      }
+      console.log(`
+        email : ${email},
+        password : ${password},
+        user : ${proUser},
+        pesudo : ${nickname},
+        pictureUser : ${pictureUser},
+        cp : ${newCP},
+        city : ${city},
+        address : ${address},
+        siret : ${newSiret},`);
       console.log(`On va s'inscrire avec email: ${email} et mdp: ${password}`);
-      // axios.post(
-      // )
-      //   .then((response) => {
-      //     console.log(response);
-      //   })
-      //   .catch((error) => {
-      //     // TODO pour afficher un message d'erreur, il faudrait ajouter une info
-      //     // dans le state, et dispatcher ici une nouvelle action
-      //     console.log(error);
-      //   });
+      axios.post('http://3.218.156.136/api/user/create',
+        {
+          email: email,
+          roles: proUser,
+          password: password,
+          pseudo: nickname,
+          avatar: pictureUser,
+          cp: newCP,
+          city: city,
+          adresse: address,
+          siret: newSiret,
+        })
+        .then((response) => {
+          console.log(response);
+          window.location = '/';
+        })
+        .catch((error) => {
+          // TODO pour afficher un message d'erreur, il faudrait ajouter une info
+          // dans le state, et dispatcher ici une nouvelle action
+          console.log(error);
+        });
       break;
     }
     default:
