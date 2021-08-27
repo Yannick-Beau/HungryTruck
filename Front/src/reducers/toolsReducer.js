647f4521ef57452b42f79d578b94d0730ee649fe
Yannick-Beau <beauyannick@gmail.com>
2021-08-27 09:21:20 +0200

    [UPDATE] add toolsReducer for open/close IconUser

---
+++ Front/src/reducers/toolsReducer.js
@@ -0,0 +1,15 @@
+//import { SEARCH_FIELD, LOAD_SEARCH } from '../actions/search';
+
+const initialState = {
+  iconUserIsOpen: false,
+};
+
+function toolsReducer(state = initialState, action = {}) {
+  switch (action.type) {
+   
+    default:
+      return state;
+  }
+}
+
+export default toolsReducer;
