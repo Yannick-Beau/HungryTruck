c521f8d06e1db546290c224bcef15156fd75dfcb
Yannick-Beau <beauyannick@gmail.com>
2021-08-27 09:28:47 +0200

    [UPDATE] add action tools for open/close IconUser

---
+++ Front/src/actions/tools.js
@@ -0,0 +1,7 @@
+// === action types
+export const CHANGE_STATUS_ICON_USER = 'CHANGE_STATUS_ICON_USER';
+
+// === action creators
+export const changeStatusIconUser = () => ({
+  type: CHANGE_STATUS_ICON_USER,
+});
