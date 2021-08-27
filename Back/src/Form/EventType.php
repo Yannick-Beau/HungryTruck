4132e3f6d62a4df2d9aad765086cdea5332a6358
Aodrën LACOT LE CLANCHE <62881216+ExoCinki@users.noreply.github.com>
2021-08-27 10:18:11 +0200

    [UPDATED] Delete Directory FRONT

--- Back/src/Form/EventType.php
+++ Back/src/Form/EventType.php
@@ -6,16 +6,21 @@ use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class EventType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('day')
-            ->add('hours')
-            ->add('hours_end')
-            ->add('place')
+            ->add('day', TextType::class, [
+                'required'   => true])
+            ->add('hours', TextType::class, [
+                'required'   => true])
+            ->add('hours_end', TextType::class, [
+                'required'   => true])
+            ->add('place', TextType::class, [
+                'required'   => true])
             ->add('foodtruck')
         ;
     }
