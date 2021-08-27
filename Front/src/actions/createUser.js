e3c6d448c77935c6a52dca7d2c327bdc1cb53766
Yannick-Beau <beauyannick@gmail.com>
2021-08-27 15:54:42 +0200

    [UPDATE] fix action save food

--- Front/src/actions/createUser.js
+++ Front/src/actions/createUser.js
@@ -26,6 +26,7 @@ export const findFood = () => ({
   type: FIND_FOOD,
 });
 
-export const saveFood = () => ({
+export const saveFood = (newValue) => ({
   type: SAVE_FOOD,
+  newValue: newValue,
 });
