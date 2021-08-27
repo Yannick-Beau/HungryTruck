f3f4f3098c1087e0e22c80295c852427a1317edc
Yannick-Beau <beauyannick@gmail.com>
2021-08-27 20:42:38 +0200

    [UPDATE] fix identation LogIn

--- Front/src/components/MyAccount/LogIn/index.js
+++ Front/src/components/MyAccount/LogIn/index.js
@@ -13,54 +13,55 @@ const LogIn = ({
   handleSubmit,
   tokenIsOk,
 }) => {
-  if(tokenIsOk) {
-    return <Redirect to="/" />; 
+  if (tokenIsOk) {
+    return <Redirect to="/" />;
   }
-  return(
-  <section className="login-section">
-    <h2 className="login-title">Connexion à HungryTruck</h2>
-    <p>Bienvenu sur HungryTruck</p>
-    <form 
-      className="login-form" 
-      onSubmit={(evt) => {
-        evt.preventDefault();
-        handleSubmit();
-      }}
-    >
-      <label className="login-label" htmlFor="email">Votre Email
-        <input
-          className="login-input"
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={email}
-          onChange={(evt) => {
-            changeField(evt.target.value, 'email');
-          }}
-        />
-      </label>
-      <label className="login-label" htmlFor="password">Votre mot de passe
-        <input
-          className="login-input"
-          type="password"
-          name="password"
-          placeholder="Mot de passe"
-          value={password}
-          onChange={(evt) => {
-            changeField(evt.target.value, 'password');
-          }}
-        />
-      </label>
-      <button type="submit" className="login-form--submit">Connexion</button>
-    </form>
-    <Link to="/new-account" className="button-Link">
-      <button type="button" className="login-createaccount">Créer un compte</button>
-    </Link>
-    <Link to="/" className="button-Link">
-      <button type="button" className="login-cancel">Retourner à l'acceuil</button>
-    </Link>
-  </section>
-)};
+  return (
+    <section className="login-section">
+      <h2 className="login-title">Connexion à HungryTruck</h2>
+      <p>Bienvenu sur HungryTruck</p>
+      <form
+        className="login-form"
+        onSubmit={(evt) => {
+          evt.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <label className="login-label" htmlFor="email">Votre Email
+          <input
+            className="login-input"
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={(evt) => {
+              changeField(evt.target.value, 'email');
+            }}
+          />
+        </label>
+        <label className="login-label" htmlFor="password">Votre mot de passe
+          <input
+            className="login-input"
+            type="password"
+            name="password"
+            placeholder="Mot de passe"
+            value={password}
+            onChange={(evt) => {
+              changeField(evt.target.value, 'password');
+            }}
+          />
+        </label>
+        <button type="submit" className="login-form--submit">Connexion</button>
+      </form>
+      <Link to="/new-account" className="button-Link">
+        <button type="button" className="login-createaccount">Créer un compte</button>
+      </Link>
+      <Link to="/" className="button-Link">
+        <button type="button" className="login-cancel">Retourner à l'acceuil</button>
+      </Link>
+    </section>
+  );
+};
 
 // == Export
 export default LogIn;
