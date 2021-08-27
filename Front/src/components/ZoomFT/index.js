b849253df8d5092512cc2b1ad2fc3d4bd09e9292
TURPINJonathan <turpin.j@hotmail.fr>
2021-08-27 14:27:45 +0200

    [UPDATE] map on food propose

--- Front/src/components/ZoomFT/index.js
+++ Front/src/components/ZoomFT/index.js
@@ -12,20 +12,20 @@ const ZoomFT = ({ trucks }) => {
   const { slug } = useParams();
   const idInt = parseInt(slug, 10);
   const truck = trucks.find((item) => item.id === idInt);
+  console.log(truck.name);
   return (
     <section className="zoom-section">
-      <h1>Mon slug {slug}</h1>
-      <h2 className="zoom-title">Nom du food truck</h2>
+      <h2 className="zoom-title">{truck.name}</h2>
       <div className="image-reseaux">
-        <img src={foodtruck} alt="nom du food truck" />
+        <img src={truck.picture} alt="nom du food truck" />
         <div className="reseaux">
-          <a href="https://fr-fr.facebook.com/">
+          <a href={truck.facebook}>
             <Facebook size="50" color="rgba(255, 255, 255, 0.7)" />
           </a>
-          <a href="https://www.instagram.com/?hl=fr">
+          <a href={truck.instagram}>
             <Instagram size="50" color="rgba(255, 255, 255, 0.7)" />
           </a>
-          <a href="https://twitter.com/?lang=fr">
+          <a href={truck.twitter}>
             <Twitter size="50" color="rgba(255, 255, 255, 0.7)" />
           </a>
         </div>
@@ -73,8 +73,9 @@ const ZoomFT = ({ trucks }) => {
         <div className="type-food">
           <h3>Les nourritures que le food truck propose</h3>
           <ul>
-            <li>Pizza</li>
-            <li>burger</li>
+            {truck.sell_type_food.map((item) => (
+              <li key={item.name}>{item.name}</li>
+            ))}
           </ul>
         </div>
         <Link
