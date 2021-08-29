2f72df1e3cea8ed2e3a46a18621c923a16d59c3c
Yannick-Beau <beauyannick@gmail.com>
2021-08-29 19:59:39 +0200

    [UPDATE] edit User WIP, info perso ok, bug food, and lack password

--- Front/src/middlewares/editUserMiddleware.js
+++ Front/src/middlewares/editUserMiddleware.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import {
   FIND_FOOD,
   FIND_USER,
+  SAVE_EDIT_USER,
   editUser,
   editPro,
   saveFood,
@@ -62,19 +63,78 @@ const editUserMiddleware = (store) => (next) => (action) => {
             ));
             console.log('foodIsLike est :', foodIsLike);
             if (foodIsLike !== undefined) {
-              console.log('la food aimer est egale a la food');
+              // console.log('la food aimer est egale a la food');
               return {
                 ...item,
                 isCheck: true,
               };
             }
+            // console.log('la food aimer est egale a la food');
             return {
               ...item,
               isCheck: false,
             };
           });
+          // console.log('on va save les food', newData);
           // on sauvegarde le nouveau tableau dans le state
           store.dispatch(saveFood(newData));
+        })
+        .catch((error) => {
+          // TODO pour afficher un message d'erreur, il faudrait ajouter une info
+          // dans le state, et dispatcher ici une nouvelle action
+          console.log(error);
+        });
+      break;
+    }
+    case SAVE_EDIT_USER: {
+      const {
+        email,
+        pseudo,
+        avatar,
+        cp,
+        city,
+        adresse,
+        siret,
+        foods,
+      } = store.getState().editUser;
+      const foodLikeFilter = foods.filter((item) => (item.isCheck));
+      const foodLikeMap = foodLikeFilter.map((item) => {
+        const newFood = {
+          name: item.name,
+        };
+        return newFood;
+      });
+      const foodLike = [
+        ...foodLikeMap,
+      ];
+      console.log('new food like', foodLike);
+      const newCP = parseInt(cp, 10);
+      const newSiret = parseInt(siret, 10);
+      console.log(`
+        email : ${email},
+        pesudo : ${pseudo},
+        pictureUser : ${avatar},
+        cp : ${newCP},
+        city : ${city},
+        address : ${adresse},`);
+      const data = {
+        email: email,
+        pseudo: pseudo,
+        avatar: avatar,
+        cp: newCP,
+        city: city,
+        adresse: adresse,
+        food_like: foodLike,
+        siret: newSiret,
+      };
+      const config = {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      };
+      axios.patch(`${URL}/api/pro/edit`, data, config)
+        .then((response) => {
+          console.log(response);
         })
         .catch((error) => {
           // TODO pour afficher un message d'erreur, il faudrait ajouter une info
