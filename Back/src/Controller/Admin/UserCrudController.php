6e79c185e2248e982e1d936d6fdcaae870f893a8
Aodrën LACOT LE CLANCHE <62881216+ExoCinki@users.noreply.github.com>
2021-09-03 13:06:58 +0200

    [UPDATED] remove useless dependencies

--- Back/src/Controller/Admin/UserCrudController.php
+++ Back/src/Controller/Admin/UserCrudController.php
@@ -9,7 +9,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
