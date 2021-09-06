63ec7ed6f53ad9ca1c39e989e215f11df30604f3
Yannick-Beau <beauyannick@gmail.com>
2021-09-06 17:09:54 +0200

    [UPDATE] fix text intro

--- Front/src/components/Main/Introduction/index.js
+++ Front/src/components/Main/Introduction/index.js
@@ -10,7 +10,7 @@ const Introduction = () => (
     <h2 id="introduction-title">Bienvenue sur HungryTruck</h2>
     <p className="introduction-text">
       HungryTruck vous permettra de visualiser les différents food
-      trucks qui sont autour de chez vous, ainsi que les menus.
+      trucks qui sont autour de chez vous.
     </p>
     <p className="introduction-text">De quoi vous régaler et dire adieu à votre balance !</p>
   </aside>
