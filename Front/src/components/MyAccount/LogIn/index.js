7f45113bc6e10a98704ab7f955d8a41208c25d98
Yannick-Beau <beauyannick@gmail.com>
2021-09-06 10:39:21 +0200

    [UPDATE] fix font

--- Front/src/components/MyAccount/LogIn/index.js
+++ Front/src/components/MyAccount/LogIn/index.js
@@ -1,6 +1,7 @@
 // == Import npm
 import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
 import PropTypes from 'prop-types';
 
 // == Import
