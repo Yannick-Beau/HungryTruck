5153d7b52f58ee821ce584403d7c2f2c0afd3351
Yannick-Beau <beauyannick@gmail.com>
2021-09-08 12:11:00 +0200

    [UPDATE] cgu

---
+++ Front/src/components/Footer/CGU/index.js
@@ -0,0 +1,186 @@
+/* eslint-disable max-len */
+// == Import npm
+import React from 'react';
+
+// == Import
+import hungryTruckLogo from '../../../assets/img/logo.png';
+
+// == Composant
+const Cgu = () => (
+  <section className="lm">
+    <div className="lm-title">
+      <h3 className="lm-title--title">
+        Conditions générales d’utilisation
+      </h3>
+      <a href="" alt="HungryTruck" className="lm-title--link">
+        HungryTruck
+      </a>
+      <img className="lm-title--logo" src={hungryTruckLogo} alt="logo hungry truck" />
+    </div>
+    <article className="lm-text">
+      <p className="lm-text--title">
+        Article 1 : Objet
+      </p>
+      <p className="lm-text--parahraph">
+        Les présentes « conditions générales d'utilisation » ont pour objet l'encadrement
+        juridique de l’utilisation du site www.hungrytruck.fr et de ses services.
+      </p>
+      <p className="lm-text--parahraph">
+        Ce contrat est conclu entre :
+      </p>
+      <p className="lm-text--parahraph">
+        Les gérants du site internet, ci-après désigné Yannick Beau, Aodrën Lacot Le Clanche, et Jonathan Turpin.
+      </p>
+      <p className="lm-text--parahraph">
+        Toute personne physique ou morale souhaitant accéder au site et à ses services, ci-après appelé «l’Utilisateur».
+      </p>
+      <p className="lm-text--parahraph">
+        Les conditions générales d'utilisation doivent être acceptées par tout Utilisateur, et son accès au site vaut acceptation de ces conditions.
+      </p>
+      <p className="lm-text--title">
+        Article 2 : Mentions Légales
+      </p>
+      <p className="lm-text--parahraph">
+        Le site www.hungrytruck.fr est édité par:
+      </p>
+      <p className="lm-text--parahraph">
+        <ul>
+          <li>Jonathan Turpin, domicilié au 41 rue de la pigacière, 14 000 CAEN</li>
+          <li>Aodrën Lacot Le Clanche, domicilié au 7 rue français, 38 270 BEAUREPAIRE</li>
+          <li>Yannick Beau, domicilié au 3 rue du 11 novembre, 21 140 SEMUR EN AUXOIS</li>
+        </ul>
+      </p>
+      <p className="lm-text--title">
+        Article 3 : Accès aux service
+      </p>
+      <p className="lm-text--parahraph">
+        L’Utilisateur du site www.hungrytruck.fr a accès aux services suivants:
+      </p>
+      <p className="lm-text--parahraph">
+        <ul>
+          <li>Informations sur les food truck enregistrés (emplacement, horaire, cartes, …)</li>
+          <li>S’enregistrer en tant que client ou professionnel</li>
+          <li>Carte des food truck aux alentours de sa position </li>
+          <li>Pouvoir se connecter à son compte </li>
+          <li>Rubriques «CGU», «FAQ», «Mentions légales», «contactez-nous», «qui sommes-nous?»  </li>
+        </ul>
+      </p>
+      <p className="lm-text--parahraph">
+        Tout Utilisateur ayant accès à internet peut accéder gratuitement et depuis n’importe où au site. Les frais supportés par l’Utilisateur pour y accéder (connexion internet, matériel informatique, etc.) ne sont pas à la charge des Éditeurs.
+      </p>
+      <p className="lm-text--parahraph">
+        Les services suivants ne sont pas accessibles pour l’Utilisateur que s’il est membre du site (c’est-à-dire qu’il soit identifié à l’aide de ses identifiants de connexion) :
+      </p>
+      <p className="lm-text--parahraph">
+        <ul>
+          <li>Modifier son profil </li>
+          <li>Supprimer son profil </li>
+        </ul>
+      </p>
+      <p className="lm-text--parahraph">
+        Le site et ses différents services peuvent être interrompus ou suspendus par les Éditeurs, notamment à l’occasion d’une maintenance, sans obligation de préavis ou de justification.
+      </p>
+      <p className="lm-text--title">
+        Article 4: Responsabilité de l'utilisateur
+      </p>
+      <p className="lm-text--parahraph">
+        L'Utilisateur est responsable des risques liés à l’utilisation de son identifiant de connexion et de son mot de passe.
+      </p>
+      <p className="lm-text--parahraph">
+        Le mot de passe de l’Utilisateur doit rester secret. En cas de divulgation de mot de passe, l’Éditeur décline toute responsabilité.
+      </p>
+      <p className="lm-text--parahraph">
+        L’Utilisateur assume l’entière responsabilité de l’utilisation qu’il fait des informations et contenus présents sur le site www.hungrytruck.fr.
+      </p>
+      <p className="lm-text--parahraph">
+        Tout usage du service par l'Utilisateur ayant directement ou indirectement pour conséquence des dommages doit faire l'objet d'une indemnisation au profit du site.
+      </p>
+      <p className="lm-text--parahraph">
+        En publiant sur le site, l’Utilisateur cède à la société éditrice le droit non exclusif et gratuit de représenter, reproduire, adapter, modifier, diffuser et distribuer sa publication, directement ou par un tiers autorisé.
+      </p>
+      <p className="lm-text--title">
+        Article 5 : Responsabilité
+      </p>
+      <p className="lm-text--parahraph">
+        Tout dysfonctionnement du serveur ou du réseau ne peut engager la responsabilité des Éditeurs.
+      </p>
+      <p className="lm-text--parahraph">
+        De même, la responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.
+      </p>
+      <p className="lm-text--parahraph">
+        Le site www.hungrytruck.fr s'engage à mettre en œuvre tous les moyens nécessaires pour garantir la sécurité et la confidentialité des données. Toutefois, il n’apporte pas une garantie de sécurité totale.
+      </p>
+      <p className="lm-text--parahraph">
+        Les Éditeurs se réserve la faculté d’une non-garantie de la fiabilité des sources, bien que les informations diffusées sur le site soient réputées fiables.
+      </p>
+      <p className="lm-text--title">
+        Article 6 : Propriété intellectuelle
+      </p>
+      <p className="lm-text--parahraph">
+        L’Utilisateur devra obtenir l’autorisation des Éditeurs du site avant toute reproduction, copie ou publication de ces différents contenus.
+      </p>
+      <p className="lm-text--parahraph">
+        Ces derniers peuvent être utilisés par les utilisateurs à des fins privées ; tout usage commercial est interdit.
+      </p>
+      <p className="lm-text--parahraph">
+        L’Utilisateur est entièrement responsable de tout contenu qu’il met en ligne et il s’engage à ne pas porter atteinte à un tiers.
+      </p>
+      <p className="lm-text--parahraph">
+        Les Éditeurs du site se réservent le droit de modérer ou de supprimer librement et à tout moment les contenus mis en ligne par les utilisateurs, et ce sans justification.
+      </p>
+      <p className="lm-text--title">
+        Article 7 : Données personnelles
+      </p>
+      <p className="lm-text--parahraph">
+        L’Utilisateur doit obligatoirement fournir des informations personnelles pour procéder à son inscription sur le site.
+      </p>
+      <p className="lm-text--parahraph">
+        L’adresse électronique (e-mail) de l’utilisateur pourra notamment être utilisée par le site www.hungrytruck.fr pour la communication d’informations diverses et la gestion du compte.
+      </p>
+      <p className="lm-text--parahraph">
+        www.hungrytruck.fr garantie le respect de la vie privée de l’utilisateur, conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés.
+      </p>
+      <p className="lm-text--parahraph">
+        En vertu des articles 39 et 40 de la loi en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit via :
+        <ul>
+          <li>Son espace personnel sur le site ;</li>
+          <li>Un formulaire de contact ;</li>
+          <li>Par mail à contact@hungrytruck.fr ;</li>
+        </ul>
+      </p>
+      <p className="lm-text--title">
+        Article 8 : Les liens hypertextes
+      </p>
+      <p className="lm-text--parahraph">
+        Les domaines vers lesquels mènent les liens hypertextes présents sur le site n’engagent pas la responsabilité des Éditeurs de www.hungrytruck.fr, qui n’a pas de contrôle sur ces liens.
+      </p>
+      <p className="lm-text--parahraph">
+        Il est possible pour un tiers de créer un lien vers une page du site www.hungrytruck.fr sans autorisation expresse des Éditeurs.
+      </p>
+      <p className="lm-text--title">
+        Article 9 : Évolution des conditions générales d’utilisation
+      </p>
+      <p className="lm-text--parahraph">
+        Le site www.hungrytruck.fr se réserve le droit de modifier les clauses de ces conditions générales d’utilisation à tout moment et sans justification.
+      </p>
+      <p className="lm-text--title">
+        Article 10 : Durée du contrat
+      </p>
+      <p className="lm-text--parahraph">
+        La durée du présent contrat est indéterminée. Le contrat produit ses effets à l’égard de l’Utilisateur à compter du début de l’utilisation du service.
+      </p>
+      <p className="lm-text--title">
+        Article 11 : Droit applicable et juridiction compétente
+      </p>
+      <p className="lm-text--parahraph">
+        Le présent contrat dépend de la législation française.
+      </p>
+      <p className="lm-text--parahraph">
+        En cas de litige non résolu à l’amiable entre l’Utilisateur et l’Éditeur, les tribunaux de [nom de ville] sont compétents pour régler le contentieux.
+      </p>
+    </article>
+  </section>
+);
+
+// == Export
+export default Cgu;
